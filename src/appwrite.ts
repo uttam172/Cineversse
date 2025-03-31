e7f757141796c06cff0192b8c2a3d644e189c048
uttam172 <uttamdobariya17@gmail.com>
2025-03-31 16:59:57 +0530

    Profile Component Created

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -12,26 +12,28 @@ const client: any = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(projectId)
 
-const database: any = new Databases(client)
+const database = new Databases(client)
 
-export const updateSearchCount = async (searchTearm: string, movie: Movie) => {
+export const updateSearchCount = async (searchTearm: string, movie: Movie[]) => {
+    console.log("appwrite file: ", searchTearm, movie)
     try {
-        const result = await database.listDocuments(databaseId, collectionId, [Query.equal('searchTearm', searchTearm)])
+        const result = await database.listDocuments(databaseId, collectionId, 
+            [Query.equal('searchTearm', searchTearm.trim().toLocaleLowerCase())])
         if (result.documents.length > 0) {
             const doc = result.documents[0]
+            const updatedCount = (doc["count"] ?? 0) + 1
             await database.updateDocument(databaseId, collectionId, doc.$id, {
-                count: doc.count + 1
+                count: updatedCount
             })
         }else {
             await database.createDocument( databaseId, collectionId, ID.unique(), {
                 searchTearm, 
                 count: 1, 
-                movie_id: 
-                movie.id, 
-                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                movie_id: movie[0].id, 
+                poster_url: `https://image.tmdb.org/t/p/w500${movie[0].poster_path}` 
             })
         }
     } catch (e) {
-        
+        console.error("Error updating search count:", e)
     }
 }
