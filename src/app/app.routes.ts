f0d749a8f1d4fef324cc5180ea307cff9e45bdba
uttam172 <uttamdobariya17@gmail.com>
2025-04-03 09:37:06 +0530

    auth removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'profile', component: ProfileComponent},
     {path: 'bookmarks', component: BookmarksComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
 ];
