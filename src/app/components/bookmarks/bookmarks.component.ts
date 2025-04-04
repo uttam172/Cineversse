2cf29a0419278cb95b8a2e111b103bd2409136dc
uttam172 <uttamdobariya17@gmail.com>
2025-04-04 17:47:16 +0530

    bug fix

--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -2,17 +2,15 @@ import { Component } from '@angular/core';
 
 import { Movie } from '../../models/movie.model';
 
-import { CardComponent } from '../card/card.component';
+// import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-bookmarks',
-  imports: [CardComponent],
+  imports: [],
   templateUrl: './bookmarks.component.html',
 })
 export class BookmarksComponent {
 
   movies: Movie[] = []
-
-  constructor() { }
 
 }
